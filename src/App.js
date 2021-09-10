b1628f60d7f93ccc21ae9147d439404cb0d1e1bf
naibas <naibas.kb@gmail.com>
2021-09-10 16:05:04 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="container">
+        <h2>ลงทะเบียนบัญชีเงินฝาก</h2>
+        <form>
+          <label>ชื่อ-สกุล</label>
+          <input type="text" class="form-control"></input>
+          <label>เลขที่บัญชี</label>
+          <input type="text" class="form-control"></input>
+          <input type="submit" value="บันทึก" class="btn btn-primary"></input>
+        </form>
+      </div>
     </div>
   );
 }
 
-export default App;
+function operation_out(){
+  return(
+    <div class="container">
+      <h2>ทำรายการโอนเงิน</h2>
+      <form>
+        <input type="text" class="form-control"></input>
+        <label>ช่องทางการโอน</label>
+        <select id="cars" class="form-control">
+          <option value="volvo">ทรูมันนี่</option>
+          <option value="saab">เงินในบัญชี</option>
+          <option value="vw">บัตรเครดิต</option>
+        </select>
+      </form>
+    </div>
+  )
+}
+
+
+function Show_Statemen(){
+  return(
+    <div class="container">
+      <h2>Statement</h2>
+      <table class="table table-hover">
+        <tr>
+          <th>เลขบัญชี</th>
+          <th>จำนวนเงินเข้า</th>
+          <th>จำนวนเงินออก</th>
+          <th>ยอดเงินคงเหลือ</th>
+        </tr>
+      </table>
+    </div>
+  );
+}
+export default operation_out;
